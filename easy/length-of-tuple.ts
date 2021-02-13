7f2a4ce55728c327bbf596651762fad0587a323f
Roberto Tatasciore <tatasciorecont@gmail.com>
2021-02-13 20:19:12 +0100

    Install prettier and add more solutions

--- easy/length-of-tuple.ts
+++ easy/length-of-tuple.ts
@@ -28,15 +28,6 @@ type Length<T extends readonly unknown[]> = T['length'];
 import { Equal, Expect } from '@type-challenges/utils';
 
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const;
-const spaceX = [
-  'FALCON 9',
-  'FALCON HEAVY',
-  'DRAGON',
-  'STARSHIP',
-  'HUMAN SPACEFLIGHT',
-] as const;
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const;
 
-type cases = [
-  Expect<Equal<Length<typeof tesla>, 4>>,
-  Expect<Equal<Length<typeof spaceX>, 5>>,
-];
+type cases = [Expect<Equal<Length<typeof tesla>, 4>>, Expect<Equal<Length<typeof spaceX>, 5>>];
